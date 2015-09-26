71837a54b8dfada65f7f64d91522e4d72c2affd3
LrVch <caden45@mail.ru>
2015-09-26 17:34:23 +0600

    initial project

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,60 @@
-var gulp = require('gulp');
-var googlecdn = require('gulp-google-cdn');
-var mainBowerFiles = require('main-bower-files');
-
-gulp.task('default', function () {
-  return gulp.src('index.html')
-    .pipe(googlecdn(require('./bower.json')))
-    .pipe(gulp.dest('dist'));
-});
-
-// todo: починить
-// note: не вытаскивает минифицированный файл из jquery и не дает в другую папку перенаправлять
-gulp.task('mainJS', function () {
-  return gulp.src(mainBowerFiles("**/*.js"))
-    .pipe(gulp.dest("dist/js"))
-});
+var gulp = require("gulp"),
+  browserSync = require("browser-sync"),
+  sass = require('gulp-sass'),
+  notify = require('gulp-notify'),
+  minifyCss = require('gulp-minify-css'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uncss = require('gulp-uncss');
 
 
-gulp.task('mainCSS', function () {
-  return gulp.src(mainBowerFiles("**/*.css"))
-    .pipe(gulp.dest("dist/css"))
+gulp.task("sass", function () {
+  return gulp.src("app/scss/style.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream())
+    .pipe(notify("Done!"));
 });
 
 
-gulp.task('mainSCSS', function () {
-  return gulp.src(mainBowerFiles("**/*.scss"))
-    .pipe(gulp.dest("dist/scss"))
+gulp.task('minifyCSS', function () {
+  return gulp.src('app/css/style.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', "ie 9"],
+      cascade: false
+    }))
+    .pipe(minifyCss())
+    .pipe(rename("style.mim.css"))
+    .pipe(gulp.dest('app/css'))
+    //.pipe(notify("min!"));
+});
+
+// неработает пока
+gulp.task('uncss', function () {
+  return gulp.src('app/css/style.min.css')
+    .pipe(uncss({
+      html: ['app/*.html']
+    }))
+    .pipe(gulp.dest('app/css/style.min.css'));
 });
 
 
+gulp.task("server", ["sass", "minifyCSS"], function () {
+
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  gulp.watch("app/scss/**/*.scss", ["sass"]);
+  gulp.watch("app/css/**/*.css", ["minifyCSS"]).on("change", browserSync.reload);
+  gulp.watch("app/css/**/*.min.css").on("change", browserSync.reload);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
+  gulp.watch("app/js/**/*.js").on("change", browserSync.reload);
+
+});
+
+
+gulp.task("default", ["server"]);

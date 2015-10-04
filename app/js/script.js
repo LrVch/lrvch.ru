ab948b762d18ca839192aefd066403577abac88c
LrVch <caden45@mail.ru>
2015-10-04 20:04:39 +0600

    Добавил показ модальнх окон на jquery

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,53 @@
+;
+(function ($) {
+  $(function () {
+    $('#btn-add-project').bind('click', function (e) {
+      e.preventDefault();
+      $('#moda-add-project').bPopup({
+        fadeSpeed: 'slow',
+        followSpeed: 300,
+        modalColor: '#000',
+      });
+    });
+  });
+
+})(jQuery);
+
+(function ($) {
+  $(function () {
+    $('#add-button').bind('click', function (e) {
+      e.preventDefault();
+      $('#moda-success-project').bPopup({
+        fadeSpeed: 'slow',
+        followSpeed: 300,
+        modalColor: '#000',
+      });
+    });
+  });
+
+})(jQuery);
+
 $(function () {
-  console.log('work good!')
+  var
+    button = $("#add-button"),
+    input = $(".form__input"),
+    inputFile = $("input[type='file']"),
+    labelFile = $(".form__input-file"),
+    inputLenght = input.length,
+    inputObj = {},
+    keyObj = "";
+
+  button.click(function (e) {
+    e.preventDefault();
+    for (var i = 0; i < inputLenght; i++) {
+      keyObj = input.eq(i).attr("name");
+      inputObj[keyObj] = input.eq(i).val();
+    }
+    console.log(inputObj);
+  });
+
+  inputFile.change(function () {
+    labelFile.text(inputFile.val().replace(/.+[\\\/]/, ""));
+  });
+
 });
